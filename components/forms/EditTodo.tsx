0d1d65af7186c2f9bf62139c9bd50afa778ef2a8
kodenamzz <kode4028@gmail.com>
2024-08-14 19:12:52 +0700

    fic dialog style

--- components/forms/EditTodo.tsx
+++ components/forms/EditTodo.tsx
@@ -77,125 +77,127 @@ const EditTodo = () => {
   if (!todoData) return null;
   return (
     <Dialog open={isShow} onOpenChange={onOpenChange}>
-      {!isEdit && (
-        <DialogContent className="sm:max-w-md">
-          <DialogHeader>
-            <DialogTitle>{todoData.title}</DialogTitle>
-            <DialogDescription>{todoData.description}</DialogDescription>
-          </DialogHeader>
-          <div className="flex items-center justify-between mt-6 ">
-            <p
-              className={`small-regular text-dark-400 flex items-center gap-1`}
-            >
-              Created By{" "}
-              <span className={`body-medium line-clamp-1`}>
-                {todoData.created_by.username}
-              </span>
-            </p>
-            <p
-              className={`small-regular text-dark-400 flex items-center gap-1`}
-            >
-              Created On{" "}
-              <span className={`body-medium line-clamp-1`}>
-                {moment(todoData.created_at).format("DD/MM/YYYY")}
-              </span>
-            </p>
-          </div>
-          <div className="mb-8">
-            <p
-              className={`small-regular text-dark-400 flex items-center gap-1`}
-            >
-              Updated On{" "}
-              <span className={`body-medium line-clamp-1`}>
-                {moment(todoData.updated_at).format("DD/MM/YYYY")}
-              </span>
-            </p>
-          </div>
-          <DialogFooter className="justify-between gap-2">
-            <DialogClose asChild className="flex-1">
-              <Button type="button">Close</Button>
-            </DialogClose>
-            <Button
-              type="button"
-              onClick={handleClickEdit}
-              variant="secondary"
-              className="flex-1"
-            >
-              Edit
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      )}
-      {isEdit && (
-        <DialogContent className="sm:max-w-md">
-          <DialogHeader>
-            <DialogTitle>Edit Todo</DialogTitle>
-            {/* <DialogDescription>Edit your todo</DialogDescription> */}
-          </DialogHeader>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-              <div className="grid gap-4 py-4">
-                <div className="grid items-center gap-4">
-                  <FormField
-                    control={form.control}
-                    name="title"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Title</FormLabel>
-                        <FormControl>
-                          <Input
-                            placeholder="title"
-                            {...field}
-                            className="col-span-3"
-                          />
-                        </FormControl>
-                        <FormMessage className="text-xs" />
-                      </FormItem>
-                    )}
-                  />
+      <DialogContent className="sm:max-w-md">
+        {!isEdit && (
+          <>
+            <DialogHeader>
+              <DialogTitle>{todoData.title}</DialogTitle>
+              <DialogDescription>{todoData.description}</DialogDescription>
+            </DialogHeader>
+            <div className="flex items-center justify-between mt-6 ">
+              <p
+                className={`small-regular text-dark-400 flex items-center gap-1`}
+              >
+                Created By{" "}
+                <span className={`body-medium line-clamp-1`}>
+                  {todoData.created_by.username}
+                </span>
+              </p>
+              <p
+                className={`small-regular text-dark-400 flex items-center gap-1`}
+              >
+                Created On{" "}
+                <span className={`body-medium line-clamp-1`}>
+                  {moment(todoData.created_at).format("DD/MM/YYYY")}
+                </span>
+              </p>
+            </div>
+            <div className="mb-8">
+              <p
+                className={`small-regular text-dark-400 flex items-center gap-1`}
+              >
+                Updated On{" "}
+                <span className={`body-medium line-clamp-1`}>
+                  {moment(todoData.updated_at).format("DD/MM/YYYY")}
+                </span>
+              </p>
+            </div>
+            <DialogFooter className="justify-between gap-2">
+              <DialogClose asChild className="flex-1">
+                <Button type="button">Close</Button>
+              </DialogClose>
+              <Button
+                type="button"
+                onClick={handleClickEdit}
+                variant="secondary"
+                className="flex-1"
+              >
+                Edit
+              </Button>
+            </DialogFooter>
+          </>
+        )}
+        {isEdit && (
+          <>
+            <DialogHeader>
+              <DialogTitle>Edit Todo</DialogTitle>
+              {/* <DialogDescription>Edit your todo</DialogDescription> */}
+            </DialogHeader>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="grid gap-4 py-4">
+                  <div className="grid items-center gap-4">
+                    <FormField
+                      control={form.control}
+                      name="title"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Title</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="title"
+                              {...field}
+                              className="col-span-3"
+                            />
+                          </FormControl>
+                          <FormMessage className="text-xs" />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="grid items-center gap-4">
+                    <FormField
+                      control={form.control}
+                      name="description"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Description</FormLabel>
+                          <FormControl>
+                            <Input
+                              multiline
+                              placeholder="description"
+                              {...field}
+                              className="col-span-3"
+                            />
+                          </FormControl>
+                          <FormMessage className="text-xs" />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
                 </div>
-                <div className="grid items-center gap-4">
-                  <FormField
-                    control={form.control}
-                    name="description"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Description</FormLabel>
-                        <FormControl>
-                          <Input
-                            multiline
-                            placeholder="description"
-                            {...field}
-                            className="col-span-3"
-                          />
-                        </FormControl>
-                        <FormMessage className="text-xs" />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-              </div>
-              <DialogFooter>
-                <Button
-                  type="button"
-                  onClick={() => setIsEdit(false)}
-                  variant="secondary"
-                  className="flex-1"
-                >
-                  Cancel
-                </Button>
-                <Button
-                  disabled={form.formState.isSubmitting}
-                  type="submit"
-                  className="flex-1"
-                >
-                  Save Changes
-                </Button>
-              </DialogFooter>
-            </form>
-          </Form>
-        </DialogContent>
-      )}
+                <DialogFooter>
+                  <Button
+                    type="button"
+                    onClick={() => setIsEdit(false)}
+                    variant="secondary"
+                    className="flex-1"
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    disabled={form.formState.isSubmitting}
+                    type="submit"
+                    className="flex-1"
+                  >
+                    Save Changes
+                  </Button>
+                </DialogFooter>
+              </form>
+            </Form>
+          </>
+        )}
+      </DialogContent>
     </Dialog>
   );
 };
